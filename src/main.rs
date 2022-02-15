a4e1d1dbf9b4f7b4d68fd46311baf4f1f3aa2178
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-02-15 17:23:04 +1100

    init

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use chrono::{DateTime, Local};
+use std::error::Error;
+use std::fs;
+use std::path::Path;
+use std::process;
+fn main() {
+    let raw = std::env::args().nth(1);
+    let default = String::from(".");
+    let arg = raw.unwrap_or(default);
+
+    if let Err(ref e) = run(Path::new(&arg)) {
+        println!("{}", e);
+        process::exit(1)
+    }
+
+    process::exit(0)
+}
+
+fn run(dir: &Path) -> Result<(), Box<dyn Error>> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+
+            if entry.path().is_dir() {
+                let dirname = entry
+                    .file_name()
+                    .into_string()
+                    .or_else(|f| Err(format!("Invalid: {:?}", f)))?;
+                let metadata = entry.metadata()?;
+                let modified: DateTime<Local> = DateTime::from(metadata.modified()?);
+
+                println!(
+                    "{:>11} | {} | {}",
+                    "Directory",
+                    modified.format("%_d %b %H:%M").to_string(),
+                    dirname
+                );
+
+                continue;
+            }
+
+            let filename = entry
+                .file_name()
+                .into_string()
+                .or_else(|f| Err(format!("Invalid: {:?}", f)))?;
+
+            let metadata = entry.metadata()?;
+            let size = metadata.len();
+            let modified: DateTime<Local> = DateTime::from(metadata.modified()?);
+
+            println!(
+                "{:>5} Bytes | {} | {}",
+                size,
+                modified.format("%_d %b %H:%M").to_string(),
+                filename
+            );
+        }
+    }
+
+    Ok(())
+}
